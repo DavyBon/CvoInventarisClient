91cbad0cd921f924acb7478358339eb74361b7e6
Malik Cornet <cornetmalik@hotmail.com>
2016-04-16 13:30:18 +0200

    Kleine css aanpassingen + namespaces aangepast van inventariscontroller, objectcontreoller, netwerkcontroller

--- CvoInventarisClient/Controllers/NetwerkController.cs
+++ CvoInventarisClient/Controllers/NetwerkController.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace CvoInventarisMalik.Controllers
+namespace CvoInventarisClient.Controllers
 {
     public class NetwerkController : Controller
     {
