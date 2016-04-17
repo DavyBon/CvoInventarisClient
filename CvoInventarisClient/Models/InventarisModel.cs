eb123ce890158d8be7e28c0182853cd5cff81979
Malik Cornet <cornetmalik@hotmail.com>
2016-04-17 20:23:12 +0200

    Models
    NetwerkModel,ObjectModel,InventarisModel

---
+++ CvoInventarisClient/Models/InventarisModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CvoInventarisClient.Models
+{
+    public class InventarisModel
+    {
+        public int Id { get; set; }
+        public LokaalModel Lokaal { get; set; }
+        public ObjectModel Object { get; set; }
+        public VerzekeringModel Verzekering { get; set; }
+        public bool IsAanwezig { get; set; }
+        public bool IsActief { get; set; }
+        public string Label { get; set; }
+    }
+}
