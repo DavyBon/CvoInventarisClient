33fd1173372bd21bdc9dea8623a6cf33312ad468
Anthony Bonnevalle <anthony.bonn@hotmail.com>
2016-05-05 10:26:27 +0200

    Controller, views en model aangepast voor Lokalen

--- CvoInventarisClient/Models/LokaalViewModel.cs
+++ CvoInventarisClient/Models/LokaalViewModel.cs
@@ -2,12 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace CvoInventarisClient.Models
 {
     public class LokaalViewModel
     {
         public List<LokaalModel> Lokaal { get; set; }
-        public List<NetwerkModel> Netwerk { get; set; }
+        public List<SelectListItem> Netwerk { get; set; }
     }
 }
