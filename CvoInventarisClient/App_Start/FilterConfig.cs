e3d85957063d57e87f82df1a7858fabd04ebd2d9
Anthony Bonnevalle <anthony.bonn@hotmail.com>
2016-05-13 14:36:49 +0200

    ErrorHandling toegepast op het project
    FilterConfig klasse toegevoegd waardoor alle controllers en
    actionmethods gebruik maken van het [HandleError] attribuut.

---
+++ CvoInventarisClient/App_Start/FilterConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CvoInventarisClient.App_Start
+{
+    // Deze klasse laat ons toe om het "HandleError" attribuut automatisch toe te passen op alle controllers en actionmethods.
+    // Deze klasse werd geregistreerd in de global.asax file.
+
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ CvoInventarisClient/Controllers/ErrorController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CvoInventarisClient.Controllers
+{
+
+    // Omdat het "HandleError" attribuut geen rekening houdt met status code 404 werd deze controller aangemaakt.
+    // In web.config werd volgende aanpassing gemaakt om naar de correcte view te verwijzen indien status code 404 optreed.
+    //
+    // <customErrors mode="On">
+    // <error statusCode = "404" redirect="~/Error/NotFound"/>
+    // </customErrors>
+    //
+    // Wanneer error statusCode 404 optreed redirecten we naar de Error controller en daarin naar de NotFound actionmethod.
+    // Deze actionmethod returned de view met dezelfde naam. Deze bevind zich in de shared folder.
+
+    public class ErrorController : Controller
+    {
+        public ActionResult NotFound()
+        {
+            return View();
+        }
+    }
+}
