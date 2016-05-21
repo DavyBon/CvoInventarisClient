b9e17807b14d76470020ebefb63524f45f5de659
Malik Cornet <cornetmalik@hotmail.com>
2016-05-21 15:33:24 +0200

    voorbeeld export

---
+++ CvoInventarisClient/Controllers/ExportController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CvoInventarisClient.Controllers
+{
+    public class ExportController : Controller
+    {
+        // GET: Export
+        public ActionResult ExportToPdf(int[] modelList, string type)
+        {
+            List<object> objects = new List<object>();
+            switch (type)
+            {
+                case "Object":
+                    DAL.TblObject tblObject = new DAL.TblObject();
+                    foreach (int id in modelList)
+                    {
+                        objects.Add(tblObject.GetById(id));
+                    }
+                    break;
+            }
+            //export code hier
+
+            return RedirectToAction("Index", type);
+        }
+        public ActionResult ExportToExcel(int[] modelList, string type)
+        {
+            List<object> objects = new List<object>();
+            switch (type)
+            {
+                case "Object":
+                    DAL.TblObject tblObject = new DAL.TblObject();
+                    foreach (int id in modelList)
+                    {
+                        objects.Add(tblObject.GetById(id));
+                    }
+                    break;
+            }
+            //export code hier
+
+            return RedirectToAction("Index", type);
+        }
+    }
+}
