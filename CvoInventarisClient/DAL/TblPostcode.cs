ecf98636dc8caf4d9a2629ba576e6af64b408ffb
Kenneth Moons <kenneth.moons1@gmail.com>
2016-05-22 17:16:08 +0200

    postcode aangemaakt

---
+++ CvoInventarisClient/DAL/TblPostcode.cs
@@ -0,0 +1,97 @@
+﻿using CvoInventarisClient.DAL.interfaces;
+using CvoInventarisClient.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace CvoInventarisClient.DAL
+{
+    public class TblPostcode : ICrudable<PostcodeModel>
+    {
+        SqlConnection connection = new SqlConnection("Data Source=92.222.220.213,1500;Initial Catalog=CvoInventarisdb;Persist Security Info=True;User ID=sa;Password=grati#s1867");
+
+        public int Create(PostcodeModel t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<PostcodeModel> GetAll()
+        {
+            List<PostcodeModel> list = new List<PostcodeModel>();
+            CampusModel campus;
+
+            try
+            {
+                using (SqlCommand cmd = new SqlCommand("TblPostcodeReadAll", connection))
+                {
+                    connection.Open();
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    SqlDataReader dr = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+
+                    while (dr.Read())
+                    {
+                        PostcodeModel postcode = new PostcodeModel();
+                        postcode.Id = (int)dr["idPostcode"];
+                        postcode.Postcode = dr["postcode"].ToString();
+                        postcode.Gemeente = dr["gemeente"].ToString();
+                        list.Add(postcode);
+                    }
+                    return list;
+                }
+            }
+            catch
+            {
+                return null;
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+        public PostcodeModel GetById(int id)
+        {
+            PostcodeModel postcode = new PostcodeModel();
+
+            try
+            {
+                using (SqlCommand cmd = new SqlCommand("TblPostcodeReadOne", connection))
+                {
+                    connection.Open();
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("idPostcode", id);
+                    SqlDataReader dr = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+
+                    while (dr.Read())
+                    {
+                        postcode.Id = (int)dr["idPostcode"];
+                        postcode.Postcode = dr["postcode"].ToString();
+                        postcode.Gemeente = dr["gemeente"].ToString();
+                    }
+                    return postcode;
+                }
+            }
+            catch
+            {
+                return null;
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+        public bool Update(PostcodeModel t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CvoInventarisClient/Models/PostcodeModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CvoInventarisClient.Models
+{
+    public class PostcodeModel
+    {
+        public int Id { get; set; }
+        public string Postcode { get; set; }
+        public string Gemeente { get; set; }
+    }
+}
