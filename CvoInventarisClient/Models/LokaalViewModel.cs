1e360bc2e7d37ec088bf7e4fb551554dc106c927
Da Bon <bon.da@oulook.be>
2016-05-22 16:00:09 +0200

    Rapportering Lokaal nieuw filter systeem

---
+++ CvoInventarisClient/Models/LokaalViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CvoInventarisClient.Models
+{
+    public class LokaalViewModel : ICloneable
+    {
+        public List<LokaalModel> Lokalen { get; set; }
+        public List<SelectListItem> Campussen { get; set; }
+
+        public object Clone()
+        {
+            return this.MemberwiseClone();
+        }
+    }
+}
