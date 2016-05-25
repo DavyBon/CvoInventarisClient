4dddb368de08482252a1c9e34c152e4cb909f73b
Kenneth Moons <kenneth.moons1@gmail.com>
2016-05-25 14:14:48 +0200

    verzekering aanpassing ordenen en refresh

--- CvoInventarisClient/Controllers/Beheer/VerzekeringController.cs
+++ CvoInventarisClient/Controllers/Beheer/VerzekeringController.cs
@@ -15,11 +15,48 @@ namespace CvoInventarisClient.Controllers
             DAL.TblVerzekering dalVerzekering = new DAL.TblVerzekering();
             return dalVerzekering.GetAll();
             }
-        public ActionResult Index()
+
+        public ActionResult Index(int? amount, string order, bool? refresh)
         {
             ViewBag.action = TempData["action"];
-            DAL.TblVerzekering dalVerzekering = new DAL.TblVerzekering();
-            List<VerzekeringModel> model = dalVerzekering.GetAll();
+            VerzekeringViewModel model = new VerzekeringViewModel();
+            if (Session["verzekeringviewwmodel"] == null || refresh == true)
+            {
+                DAL.TblVerzekering dalVerzekering = new DAL.TblVerzekering();
+                List<VerzekeringModel> verzekeringen = dalVerzekering.GetAll();
+                model.Verzekeringen = verzekeringen;
+                
+            }
+            else
+            {
+                model = (VerzekeringViewModel)Session["verzekeringviewwmodel"];
+            }
+            Session["verzekeringviewwmodel"] = model.Clone();
+            if (amount == null)
+            {
+                model.Verzekeringen = model.Verzekeringen.Take(100).ToList();
+                ViewBag.amount = "100";
+            }
+            else
+            {
+                model.Verzekeringen = model.Verzekeringen.Take((int)amount).ToList();
+                ViewBag.amount = amount.ToString();
+            }
+
+            if (!string.IsNullOrWhiteSpace(order))
+            {
+                if (order.Equals("Meest recent"))
+                {
+                    model.Verzekeringen.Reverse();
+                }
+                ViewBag.ordertype = order.ToString();
+            }
+            else
+            {
+                ViewBag.ordertype = "Oudst";
+            }
+
+            ViewBag.Heading = this.ControllerContext.RouteData.Values["controller"].ToString() + " (" + model.Verzekeringen.Count() + ")";
             return View(model);
             
         }
@@ -38,15 +75,18 @@ namespace CvoInventarisClient.Controllers
         // GET: Inventaris/Edit/5
         public ActionResult Edit(int id)
         {
+            VerzekeringViewModel model = new VerzekeringViewModel();
             DAL.TblVerzekering dalVerzekering = new DAL.TblVerzekering();
             VerzekeringModel verzekering = dalVerzekering.GetById(id);
-            return View(verzekering);
+            model.Verzekeringen.Add(verzekering);
+            return View(model);
         }
 
         // POST: Inventaris/Edit/5
         [HttpPost]
         public ActionResult Edit(int id, FormCollection collection)
         {
+            
             DAL.TblVerzekering dalVerzekering = new DAL.TblVerzekering();
             VerzekeringModel verzekering = new VerzekeringModel();
             verzekering.Id = Convert.ToInt16(Request.Form["idVerzekering"]);
---
+++ CvoInventarisClient/Models/VerzekeringViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CvoInventarisClient.Models
+{
+    public class VerzekeringViewModel : ICloneable
+    {
+        public List<VerzekeringModel> Verzekeringen { get; set; }
+        public object Clone()
+        {
+            return this.MemberwiseClone();
+        }
+    }
+}
