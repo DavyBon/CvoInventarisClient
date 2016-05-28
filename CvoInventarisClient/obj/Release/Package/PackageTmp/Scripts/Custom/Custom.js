04311289429a40d71bc4fc78c145cd2fa89c9388
Malik Cornet <cornetmalik@hotmail.com>
2016-05-28 13:25:45 +0200

    inventaris controller aanpassingen ivm laadtijd, edit checkbox fix

---
+++ CvoInventarisClient/obj/Release/Package/PackageTmp/Scripts/Custom/Custom.js
@@ -0,0 +1,13 @@
+﻿
+$(document).ready(function () {
+    (function ($) {
+        $('#filter').keyup(function () {
+            var rex = new RegExp($(this).val(), 'i');
+            $('.search tr').hide();
+            $('.search tr').filter(function () {
+                return rex.test($(this).text());
+            }).show();
+        })
+    }(jQuery));
+});
+
