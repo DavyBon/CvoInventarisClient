f47d920bc261611dac83a5b7912249088a88e933
Da Bon <bon.da@oulook.be>
2016-05-31 16:17:54 +0200

    Unit tests voor DALutil

---
+++ CvoInventarisClient/UnitTests/DALutilTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CvoInventarisClient.App_Code;
+
+namespace CvoInventarisClient.UnitTests
+{
+    [TestClass]
+    public class DALutilTest
+    {
+        [TestMethod]
+        public void checkIntForDBNUllTestShouldReturnDBNullValueWhenPassNull()
+        {
+            object result = DALutil.checkIntForDBNUll(null);
+            Assert.AreEqual(DBNull.Value, result);
+        }
+
+        [TestMethod]
+        public void checkIntForDBNUllTestShouldReturnIntWhenPassInt()
+        {
+            object result = DALutil.checkIntForDBNUll(5);
+            Assert.AreEqual(5, result);
+        }
+
+        [TestMethod]
+        public void checkDecimalForDBNUllTestShouldReturnDBNullValueWhenPassNull()
+        {
+            object result = DALutil.checkDecimalForDBNUll(null);
+            Assert.AreEqual(DBNull.Value, result);
+        }
+
+        [TestMethod]
+        public void checkDecimalForDBNUllTestShouldReturnDecimalWhenPassDecimal()
+        {
+            object result = DALutil.checkDecimalForDBNUll(3.5M);
+            Assert.AreEqual(3.5M, result);
+        }
+    }
+}
